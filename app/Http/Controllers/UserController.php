03c9546ed1b31ff977652cb8794dd73eba8c19d5
lercc <lercc.en@gmail.com>
2021-01-10 18:02:24 -0500

    Make user validations and show inputs errors on UserController and user.index view

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,6 +18,12 @@ class UserController extends Controller
 
     public function store(Request $request) 
     {
+        $request->validate([
+            'name' => 'required',
+            'email' => ['required', 'email', 'unique:users'],
+            'password' => ['required', 'min:8 '],
+        ]);
+
         User::create([
             'name' => $request->name,
             'email' => $request->email,
