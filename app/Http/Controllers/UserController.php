3bd2dc6657ce0474514e4e9a3167b5a28a91b4a2
lercc <lercc.en@gmail.com>
2021-01-10 16:20:56 -0500

    README: tinker/factory command to create fake data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,15 +7,30 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index() {
+    public function index() 
+    {
         $users = User::latest()->get();
+
+        return view('users.index', [
+            'users' => $users
+        ]);
     }
 
-    public function store() {
+    public function store(Request $request) 
+    {
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password
+        ]);
 
+        return back();
     }
 
-    public function destroy() {
+    public function destroy(User $user) 
+    {
+        $user->delete;
 
+        return back();
     }
 }
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles Boostrap v-5.0 -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous"> 
+    </head>
+    <body>
+       <div clas="container">
+          <div class="row">
+            <div class="col-sm-8 mx-auto">
+              
+              
+              
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Email</th>
+                    <th>&nbsp;</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  @foreach ($users as $user)
+                    <tr>
+                      <td>{{ $user->id }}</td>
+                      <td>{{ $user->name }}</td>
+                      <td>{{ $user->email }}</td>
+                      <td> ELIMINAR </td>
+                    </tr>
+                  @endforeach
+                </tbody>
+              </table>
+            </div>
+          </div>
+       </div>
+    </body>
+</html>
